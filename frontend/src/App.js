d5e9a1a11f4b148139b4c13475d4d4235691f0a7
dantan2000 <tan.da@husky.neu.edu>
2022-08-03 13:42:59 -0400

    Added routes for Routes, Stops, and Departures componenets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Departures from './components/Departures';
+import NotFound from './components/NotFound';
+import Stops from './components/Stops'
+import TRoutes from './components/TRoutes'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <BrowserRouter>
+      <div className="container">
+          <Routes>
+            <Route path='/' exact={true} element={<TRoutes/>}/>
+            <Route path='/:routeId' element={<Stops/>}/>
+            <Route path='/:routeId/:stopId/:directionId' element={<Departures/>}/>
+            <Route path='*' element={<NotFound/>}/>
+          </Routes>
+        </div>
+    </BrowserRouter>
+  </>;
 }
 
 export default App;
